5797a8128a721d120e522fb66736495e53269f92
sonicracing <ShShah@smail.pembinatrails.ca>
2024-01-22 18:07:54 -0600

    Changes to be committed: 	deleted:    fortnite.html 	modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-let fullscreen = document.getElementsByClassName("full screen");
+let gameframe = document.getElementsByClassName("game");
+function fortnite() {
+  alert("hi");
+  gameframe.src = "https://1v1.school/";
+  gameframe.display = "block";
+}
